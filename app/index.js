717c2243d0e67367aeedf3dbe9f6d07f27dd0f4c
Mauricio Bonetti <bonetti.mauricio@gmail.com>
2017-01-19 18:39:33 +0000

    First property used

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require("react");
+var ReactDOM = require("react-dom");
 
 var Hello = React.createClass({
   render: function () {
     return (
+      <div>
       <div>Hello ReactJS Program!</div>
+      <div>Welcome, {this.props.username}</div>
+      </div>   
     )
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(<Hello username="Mauricio"/>, document.getElementById("app"));
